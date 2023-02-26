6ac84ca38481b27c6dad55ab67b78c25957943cb
raghualajpur <2010030005@klh.edu.in>
2023-02-26 14:16:38 +0530

    connected

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const mongoose=require("mongoose");
+const dotenv = require("dotenv");
 const express=require("express")
 const app=express()
 
+dotenv.config()
+process.env
 
 
 app.use(express.static("frontend"))
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/frontend/index.html")
 })
-app.listen(3000,function(){
-    console.log("Server at http://localhost:3000")
 
+mongoose.set("strictQuery", true)
+mongoose.connect(process.env.MONGOOS,function(err){
+    if(err){
+        console.error(err)
+    }else{
+        console.log("DB Connected Successfully")
+        app.listen(3000,function(){
+            console.log("Server at http://localhost:3000")
+        
+        })
+    }
 })
-
