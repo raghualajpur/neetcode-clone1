0a852b2a56820406c730e027a81b87d16cc94cd2
raghualajpur <2010030005@klh.edu.in>
2023-02-26 10:44:25 +0530

    Pushed Html

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express=require("express")
 const app=express()
+app.use(express.static("frontend"))
+
 
 app.get("/",function(req,res){
     res.send("Hello World")
+})
+app.get("/html",function(req,res){
+    res.sendFile(__dirname+"/frontend/index.html")
 })
 app.get("/myName",function(req,res){
     res.send("Alajpur Raghu")
 })
 app.listen(3000,function(){
     console.log("Server at http://localhost:3000")
-    
+
 })
 
